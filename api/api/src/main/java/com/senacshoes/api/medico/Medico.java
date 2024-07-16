4544b8b4dc54f5004fb26f0630ff256d6e078c3f
Leandro Ferrarez <ferrarezleandro@gmail.com>
2024-07-15 21:29:43 -0300

    Primeira persistência dos dados

---
+++ api/api/src/main/java/com/senacshoes/api/medico/Medico.java
@@ -0,0 +1,15 @@
+package com.senacshoes.api.medico;
+
+import com.senacshoes.api.endereco.Endereco;
+
+public class Medico {
+    private Long id;
+    private String nome;
+    private String email;
+    private String crm;
+    private Especialidades especialidades;
+    private Endereco endereco;
+    
+}
+
+
